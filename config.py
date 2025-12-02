764915b97bb7bb2aa70fe1cd754c0d662c5509d6
jingyunn0909 <keecy-cm22@student.tarc.edu.my>
2025-12-02 14:15:03 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
-customhost = "database-1.chauwtk5goof.us-east-1.rds.amazonaws.com"
+customhost = "employee.c0fawwehbdwv.us-east-1.rds.amazonaws.com"
 customuser = "aws_user"
-custompass = "Bait3273"
+custompass = "admin123"
 customdb = "employee"
-custombucket = "lowchoonkeat-employee1"
+custombucket = "keechingyun-employee"
 customregion = "us-east-1"
 
